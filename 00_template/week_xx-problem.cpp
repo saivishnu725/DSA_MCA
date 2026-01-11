71251404fc13ea68f0b3ac527de6c4b203cbe926
Sai Vishnu <saivishnu725@gmail.com>
2026-01-11 11:12:46 +0530

    00 template

---
+++ 00_template/week_xx-problem.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+// main
+int main()
+{
+
+	int row, col;
+
+	cout << "Enter the row: ";
+	cin >> row;
+	cout << "Enter the column: ";
+	cin >> col;
+
+	cout << "hello world" << endl;
+	return 0;
+}
